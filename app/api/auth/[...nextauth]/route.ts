77f11d6c3d6ec44728a713fdb507b93e8e9e340d
Zetomis <duytan28042006@gmail.com>
2023-09-10 19:59:20 +0700

    add auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: AuthOptions = {
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        }),
+    ],
+    callbacks: {},
+    secret: process.env.SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as POST, handler as GET };
